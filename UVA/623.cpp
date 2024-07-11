4f3999679b6b961d7c0d6dc1a5409b5b8f7344fd
DDK1127 <derek61003@gmail.com>
2024-07-11 22:18:48 +0800

    500!

---
+++ UVA/623.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int num;
+    vector<vector<int> > vv(1001, vector<int>(3000));
+
+    while(cin >> num){
+        cout << vv.size() << endl;
+        cout << num <<"!" << endl;
+    }
+    return 0;
+}
