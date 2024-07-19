070a586a17dba3b7960a08ef5c65e5774c711262
DDK1127 <derek61003@gmail.com>
2024-07-20 02:10:01 +0800

    finished Insert, search.

--- DSA/Binary_Search_Tree.cpp
+++ DSA/Binary_Search_Tree.cpp
@@ -10,7 +10,8 @@ class TreeNode{
         TreeNode* left;
         TreeNode* right;
         TreeNode():left(nullptr), right(nullptr), s(""), value(0){};
-        TreeNode(int n, string s):left(nullptr), right(nullptr), s(s), value(n){}; 
+        TreeNode(int n, string s):left(nullptr), right(nullptr), s(s), value(n){};
+        TreeNode(TreeNode*node):left(node->left), right(node->right), s(node->s), value(node->value){}; 
         string get_str(){return this->s;}
         int get_value(){return this->value;}
         friend class Binary_Search_Tree;
@@ -20,22 +21,53 @@ class Binary_Search_Tree{
     public:
         TreeNode *root;
         Binary_Search_Tree():root(nullptr){};
-        TreeNode* BST_Create(TreeNode*, int input_n, string input_s);
-        void BST_insert(int input_n, string input_s);
+        TreeNode* BST_Create(int n, string s);
+        bool BST_Search(int n);
+        TreeNode* BST_insert(TreeNode*& root, int n, string s); // recusion insertion.
+        void BST_insert(int n, string s); // sequence insertion.
         bool is_Empty(TreeNode* node){return node == nullptr;};
         bool is_Leaf(TreeNode* node){return node->left == nullptr && node->right == nullptr;};
         int Node_number(TreeNode* root);
 };
 
-TreeNode* Binary_Search_Tree::BST_Create(TreeNode* ,int input_n, string input_s){
+TreeNode* Binary_Search_Tree::BST_Create(int n, string s){
     if(root == nullptr)
-        root = new TreeNode(input_n, input_s);
+        root = new TreeNode(n, s);
     else
         cout << "Root alredy have value !!!" << endl;
     cout << "value = " << root->get_value() << ", string = " << root->get_str() << endl;
 };
 
-// void Binary_Search_Tree::BST_insert(int input_n, string input_s){};
+bool Binary_Search_Tree::BST_Search(int n){
+    TreeNode* current = root;
+
+    while(current != nullptr && current->value != n){
+        if(current->value < n)
+            current = current->right;
+        else
+            current = current->left;
+    }
+
+    if(current == nullptr || current->value != n)
+        return false;
+    else{
+        cout << "find successful!, is => " << current->s << endl;
+        return true;
+    }
+};
+
+TreeNode* Binary_Search_Tree::BST_insert(TreeNode*& root, int n, string s){
+    if(root == nullptr)
+        root =  new TreeNode(n, s);
+    else if(root->value < n)
+        BST_insert(root->right, n, s);
+    else    
+        BST_insert(root->left, n, s);
+};
+
+void Binary_Search_Tree::BST_insert(int n, string s){
+    //
+};
 
 int Binary_Search_Tree::Node_number(TreeNode* root){
     if(!is_Empty(root))
@@ -46,7 +78,10 @@ int Binary_Search_Tree::Node_number(TreeNode* root){
 
 int main(){
     Binary_Search_Tree B1;
-    B1.BST_Create(B1.root, 56, "oewugfwoe");
-    B1.BST_Create(B1.root, 9999, "BBBBBBBBig BBBBBBBoos");
+    B1.BST_Create(9999, "BBBBBBBBig BBBBBBBoos");
+    B1.BST_insert(B1.root, 8841, "Ba");
+    B1.BST_insert(B1.root, 00054, "g");
+    cout << "number of Node : " << B1.Node_number(B1.root) << endl;
+    B1.BST_Search(8840) ? cout << "Find!" << endl : cout << "Not Find~" << endl;
     return 0;
 }
