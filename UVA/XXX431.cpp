584b9ddb36e6eee23954655f119dffe5ce1955cd
DDK1127 <derek61003@gmail.com>
2024-10-06 21:11:12 +0800

    output order not correct...

--- UVA/XXX431.cpp
+++ UVA/XXX431.cpp
@@ -1,67 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct info{
-    int s, t;
-    string d;
+struct info {
+    int score, time;
+    string description;
 };
 
-info make_info(int s, int t, string d){
-    info i = {s, t, d};
-    return i;
-};
-
-void Print(vector<info> v, int &ts, int &tt){
-    for(int i = 0; i < v.size(); i++){
-        ts += v[i].s;
-        tt += v[i].t;
+// Print function to display the selected evidence and totals
+void Print(vector<info>& selected, int total_score, int total_time) {
+    if (selected.empty()) {
+        cout << "There is not enough time to present any evidence. Drop the charges." << endl;
+        return;
     }
+
     cout << "Score  Time  Description" << endl;
     cout << "-----  ----  -----------" << endl;
-    for(int i = 0; i < v.size(); i++)
-        cout << setw(3) << v[i].s << "    " << setw(3) << v[i].t << "  " << v[i].d << endl;
-    cout << "Total score: " << ts << " points" << endl;
-    cout << " Total time: " << tt << " hours" << endl;
-}   
+    
+    for (int i = 0; i < selected.size(); ++i)
+        cout << std::setw(3) << selected[i].score << "    " << setw(3) << selected[i].time << "   " << selected[i].description << std::endl;
+    cout << "\nTotal score: " << total_score << " points" << endl;
+    cout << "Total time: " << total_time << " hours" << endl;
+}
 
-int main(){
-    int t, score, time, limit;
-    string description;
-    cin >> t;
+// Main function that handles the dynamic programming solution
+void SolveCase(int allowed_time, vector<info>& evidences) {
+    int n = evidences.size();
 
-    while (t-- > 0){
-        int total_score = 0, total_time = 0;
-        cin >> limit;
-        vector <info> vv;
-        vector<int> dp(limit, 0);
-        vector<vector<bool> > p(101, vector<bool>(limit, 0));
-        while(cin >> score >> time){
-            cin.ignore();
-            getline(cin, description);
-            vv.push_back(make_info(score, time, description));
-            // vv.push_back( {score, time, description} );
-        }
-        // for(int i = 0; i < vv.size(); i++){
-        //     cout << "score = " << vv[i].s << ", time = " << vv[i].t << ", des = " << vv[i].d << endl;
-        // }
-        for(int i = 1; i < limit; i++){
-            for(int j = 0; j < vv.size(); j++){
-                if(i > vv[j].t){
-                    if(dp[i-vv[j].t] + vv[j].s > dp[i])
-                        dp[i] = dp[i-vv[j].t] + vv[j].s;
-                        p[i][j] = true;
-                }
+    // DP array: dp[i] will store the maximum score achievable with i hours of time
+    vector<int> dp(allowed_time + 1, 0);
+    // trace[i][j] will track whether the jth item was selected when time was i
+    vector<vector<bool>> trace(n, vector<bool>(allowed_time + 1, false));
+
+    // Dynamic programming to solve the 0/1 knapsack problem
+    for (int i = 0; i < n; ++i) {
+        for (int t = allowed_time; t >= evidences[i].time; --t) {
+            if (dp[t - evidences[i].time] + evidences[i].score > dp[t]) {
+                dp[t] = dp[t - evidences[i].time] + evidences[i].score;
+                trace[i][t] = true;
             }
         }
-        vector<info> select;
-        int l = limit;
-        for(int i = 0; i < vv.size(); i++){
-            if(p[i][l]){
-                select.push_back(vv[i]);
-                l -= vv[i].t;
-            }
-        }
-        Print(select, total_score, total_time);
     }
+
+    // Backtrack to find the selected evidences
+    vector<info> selected;
+    int total_score = dp[allowed_time];
+    int total_time = 0;
+    int remaining_time = allowed_time;
+
+    for (int i = n - 1; i >= 0 && remaining_time > 0; --i) {
+        if (trace[i][remaining_time]) {
+            selected.push_back(evidences[i]);
+            remaining_time -= evidences[i].time;
+            total_time += evidences[i].time;
+        }
+    }
+
+    // Print the result
+    Print(selected, total_score, total_time);
+}
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore newline after the number of test cases
+
+    while (t--) {
+        int allowed_time;
+        cin >> allowed_time;
+        cin.ignore(); // Ignore newline after the allowed time
+
+        vector<info> evidences;
+        string line;
+
+        // Read evidence data
+        while (getline(cin, line) && !line.empty()) {
+            int score, time;
+            size_t pos1 = line.find(' ');  // First space between score and time
+            size_t pos2 = line.find(' ', pos1 + 1);  // Second space between time and description
+
+            score = stoi(line.substr(0, pos1));
+            time = stoi(line.substr(pos1 + 1, pos2 - pos1 - 1));
+            string description = line.substr(pos2 + 1);
+
+            evidences.push_back({score, time, description});
+        }
+
+        // Solve the case
+        SolveCase(allowed_time, evidences);
+
+        if (t > 0) {
+            cout << endl;  // Blank line between test cases
+        }
+    }
+
     return 0;
 }
